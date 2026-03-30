7b9918368065215e64eecd22933bce2efaae5348
ShevchenkoRV <237223@kisit.kneu.edu.ua>
2026-03-30 19:00:30 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+import { SubmitWorkUseCase } from "./application/submit-work";
+import { InMemoryRepository } from "./infrastructure/in-memory-repo";
 
+const repo = new InMemoryRepository();
+const submitService = new SubmitWorkUseCase(repo);
+
+// Спроба 1: Все правильно
+submitService.execute("Мій проект", "IT", "Студент")
+  .catch(err => console.error(err.message));
+
+// Спроба 2: Неправильна категорія (має видати помилку)
+submitService.execute("Малюнок", "Music", "Студент")
+  .catch(err => console.error(err.message));
