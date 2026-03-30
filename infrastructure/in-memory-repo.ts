3acf15f2abd18e80b4c42af64992f43e89d56e12
ShevchenkoRV <237223@kisit.kneu.edu.ua>
2026-03-30 19:00:07 +0300

    Update in-memory-repo.ts

--- infrastructure/in-memory-repo.ts
+++ infrastructure/in-memory-repo.ts
@@ -1,1 +1,10 @@
+import { CompetitionWork } from "../domain/competition-work";
+import { IWorkRepository } from "../application/ports";
 
+export class InMemoryRepository implements IWorkRepository {
+  private data: CompetitionWork[] = [];
+  async save(work: CompetitionWork) {
+    this.data.push(work);
+    console.log(`✅ Робота "${work.title}" успішно прийнята на конкурс!`);
+  }
+}
