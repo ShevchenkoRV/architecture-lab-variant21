aa337a850d433ea52196e1662514917445a08187
ShevchenkoRV <237223@kisit.kneu.edu.ua>
2026-03-30 18:58:31 +0300

    Update competition-work.ts

--- domain/competition-work.ts
+++ domain/competition-work.ts
@@ -1,1 +1,9 @@
+export const VALID_CATEGORIES = ['Art', 'Science', 'IT'];
 
+export interface CompetitionWork {
+  id: string;
+  title: string;
+  category: string;
+  author: string;
+  submittedAt: Date;
+}
